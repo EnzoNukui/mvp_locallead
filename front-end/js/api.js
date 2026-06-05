473db92001bd28507f72013f8d9615595527bac9
Enzo Nukui <enzonukui2007@gmail.com>
2026-06-05 02:04:27 -0300

    feat: alterando o que era necessario para fazer o deploy do backend

--- front-end/js/api.js
+++ front-end/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://locallead-api.onrender.com";
 
 async function buscarStatus() {
     const resposta = await fetch(`${API_BASE_URL}/status`);
